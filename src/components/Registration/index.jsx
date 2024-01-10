3ba21f7ce52cd9e74a4349caee00122284bfb4e5
LL <liaposhchenkol@gmail.com>
2024-01-10 22:55:13 +0100

    Add Registration

---
+++ src/components/Registration/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useState } from 'react';
+import './style.css';
+
+export const Registration = () => {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(user);
+        setUser({
+          ...user,
+          username: '',
+          email: '',
+          password: '',
+          passwordConfirm: '',
+        });
+      }}
+    >
+      <h1>REGISTRATION</h1>
+      <label>
+        Email Address
+        <input
+          type="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+      </label>
+      <br />
+      <label>
+        User Name
+        <input
+          type="text"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+        />
+      </label>
+      <br />
+      <label>
+        Password
+        <input
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+      </label>
+      <br />
+      <label>
+        Confirm Password
+        <input
+          value={user.passwordConfirm}
+          onChange={(e) =>
+            setUser({ ...user, passwordConfirm: e.target.value })
+          }
+        />
+      </label>
+      <button type="submit">REGISTER</button>
+    </form>
+  );
+};
